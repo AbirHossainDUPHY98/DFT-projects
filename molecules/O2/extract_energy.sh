9f7e7b6b4cdc425fbd3b77e2fc514f3a59af593a
AbirHossainDUPHY98 <m.abirhossain619@gmail.com>
2025-07-23 08:10:27 +0600

    Remove nested molecules/O2 folder

--- molecules/O2/extract_energy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "# BondLength(Å)    TotalEnergy(Ry)" > energy.dat
-
-for bond in $(seq 1.0 0.1 2.0); do
-    energy=$(grep "!    total energy" o2_${bond}.out | awk '{print $5}')
-    printf "%4.1f           %s\n" "$bond" "$energy" >> energy.dat
-done
